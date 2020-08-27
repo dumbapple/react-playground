6a1217f5ee59c4f0f46a7f089a659c0b96c91899
Andrew Chang <ac.weichuan@gmail.com>
2020-08-27 13:52:14 -0700

    Add files via upload

---
+++ ContactDisplayer/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CardList from './CardList';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots'; // Have to destructure this because it is not export default
+
+ReactDOM.render(
+  // Think of props like passing in arguments that will be available for component to use
+  <div>
+    <CardList robots={robots}/>
+  </div>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
