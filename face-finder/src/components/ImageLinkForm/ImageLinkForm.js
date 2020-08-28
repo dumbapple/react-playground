5f712c85b0465f852681a4d4dd0d853a3e3783f6
Andrew Chang <ac.weichuan@gmail.com>
2020-08-28 11:57:58 -0700

    Logo component

---
+++ face-finder/src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+// Simple component with no state, so can just use function syntax
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className='f3'></p>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
