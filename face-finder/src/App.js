ae9771d64bc11d56101ab89dd29e777eb54e3d28
Andrew Chang <ac.weichuan@gmail.com>
2020-08-28 12:58:52 -0700

    More styling

--- face-finder/src/App.js
+++ face-finder/src/App.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
@@ -10,6 +25,7 @@ class App extends Component {
       <div>
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/*
         <FaceRecognition />} */}
--- face-finder/src/components/ImageLinkForm/ImageLinkForm.js
+++ face-finder/src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 
 // Simple component with no state, so can just use function syntax
 const ImageLinkForm = () => {
     return (
-        <div>
-            <p className='f3'></p>
+        <div className='tc'>
+            <p className='f3'>
+                {`Give a picture, and I will find any faces in it!`}
+            </p>
+
+            <div className='form ma4 pa4 br3 shadow-5' >
+                <input className='f4 pa2 w-70 center' type='text' />
+                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            </div>
         </div>
+
+
+
     );
 }
 
